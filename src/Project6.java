b5b91557b93d8f1a1d4ecceba5c18ce08bdce64b
aldwinverzosa <averzosa@cpp.edu>
2018-11-04 22:47:42 -0600

    Initial commit

---
+++ src/Project6.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ *
+ *
+ * @author Aldwin
+ */
+public class Project6 {
+public static float getAverage(int x[]){
+    float total=0;
+    for (int i=0; i<x.length;i++)
+        total+=x[i];
+    
+    return total/x.length;
+}
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x[] = new int[6];
+        
+        int []y={10,20,30};
+    }
+    
+}
